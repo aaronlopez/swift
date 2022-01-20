cd6eb558595cb148edde9b358f5c2aaff7dcd1a1
Aaron Lopez <aaron.lopez@wizeline.com>
2022-01-20 12:33:17 +0000

    Initial Commit

---
+++ Shared/cursoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursoApp.swift
+//  Shared
+//
+//  Created by aaron lopez on 20/1/22.
+//
+
+import SwiftUI
+
+@main
+struct cursoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
